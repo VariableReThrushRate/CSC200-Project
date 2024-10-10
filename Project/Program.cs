ddab7d257a95eae95fc8bd7ca33d4b1a66b63e64
Benjamin Gress <bgpilot16@gmail.com>
2024-10-10 15:54:43 -0700

    Fleshed out classes some more

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,7 +7,47 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public class Airport
+    {
+        public string Name;
+        //IE, Seattle International is SEA. We can google these for our data.
+        public string callsign;
+        private CLatLng coords;
+        public Airport(string name, string callsign, double latitude, double longitude)
+        {
+            Name = name;
+            this.callsign = callsign;
+            this.coords = new CLatLng(latitude, longitude);
+        }
+        public Airport(string name, string callsign, CLatLng coords)
+        {
+            Name = name;
+            this.callsign = callsign;
+            this.coords = coords;
+        }
+        public CLatLng getCoords() { return coords; }
+        //Functionality to impliment:
+        //Way to add flights
+        //Way to remove flights
+        //Maybe track capacity? We can use arbitrary values for that. IE How many 727s we have or some shit idk
+    }
+    public class Aircraft
+    {
+        //A fine selection of aircraft to choose from. When constructing, I'll make it so that it assigns how much fuel is left per plane.
+        private enum ptype
+        {
+            AirbusA220,
+            AirbusA300,
+            AirbusA380,
+            AirbusA310,
+            Boeing727,
+            Boeing737,
+            Boeing777,
+            Boeing787,
+            Boeing747,
 
+        }
+    }
     public class CLatLng
     {
         public double Lat { get; private set; }
