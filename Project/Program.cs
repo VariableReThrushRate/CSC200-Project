62d1e4b655f97ec23803d4cdf1a352803704f55c
Benjamin Gress <bgpilot16@gmail.com>
2024-10-10 15:28:20 -0700

    Began class implimentation

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,4 +7,25 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    public class CLatLng
+    {
+        public double Lat { get; private set; }
+        public double Lng { get; private set; }
+        public double dist { get; private set; }
+
+
+        public CLatLng(double lat, double lng, double dist)
+        {
+            this.Lat = lat;
+            this.Lng = lng;
+            this.dist = dist;
+        }
+        public CLatLng(double lat, double lng)
+        {
+            this.Lat = lat;
+            this.Lng = lng;
+            this.dist = 1;
+        }
+    }
 }
